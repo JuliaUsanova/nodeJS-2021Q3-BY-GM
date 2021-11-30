4abbe905598e8ae0aa5d1e3444b06b86930963ba
julia.usanova <julia.usanova@refinitiv.com>
2021-11-30 21:05:33 +0300

    Synchronize test-data and sequelize User model

--- src/jobs/test-data.sql
+++ src/jobs/test-data.sql
@@ -1,20 +1,24 @@
-CREATE TABLE IF NOT EXISTS Users (
-    id VARCHAR ( 255 ) PRIMARY KEY,
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+
+CREATE TABLE IF NOT EXISTS "Users" (
+    id VARCHAR ( 255 ) DEFAULT uuid_generate_v4(),
 	login VARCHAR ( 255 ) UNIQUE NOT NULL,
 	password VARCHAR ( 255 ) NOT NULL,
 	age INTEGER NOT NULL,
 	isDeleted BOOLEAN DEFAULT FALSE,
-	createdAt TIMESTAMP WITH TIME ZONE NOT NULL,
-    updatedAt TIMESTAMP WITH TIME ZONE NOT NULL,
+	createdAt TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT clock_timestamp(),
+    updatedAt TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT clock_timestamp(),
+    PRIMARY KEY (id),
 	CHECK(password ~ '^[A-Za-z0-9]+$'),
 	CHECK (age >= 4 AND age <= 130)
 );
 
-INSERT INTO Users(login, password, age)
+INSERT INTO "Users"(login, password, age)
 SELECT
   'user_' || seq || (RANDOM() * seq) AS login,
   md5(RANDOM()::TEXT) AS password,
   (seq + 4) AS age
 FROM GENERATE_SERIES(1, 10) seq;
 
-SELECT * FROM Users;
+SELECT * FROM "Users";
